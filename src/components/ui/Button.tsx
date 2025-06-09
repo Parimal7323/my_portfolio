e5052d7769d35e72f5d186182561e126c5002259
Aryan Mishra <aryan@ARYANs-MacBook-Air.local>
2025-06-09 18:38:30 +0530

    Add icon size variant to Button component

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -16,6 +16,7 @@ const buttonVariants = cva(
         default: 'h-10 px-4 py-2',
         sm: 'h-8 px-3',
         lg: 'h-12 px-8',
+        icon: 'h-10 w-10 p-2',
       },
     },
     defaultVariants: {
